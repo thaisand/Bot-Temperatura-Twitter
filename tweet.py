2e14ce746b5a36763db61b1bf8c13c4795d156f7
Thais Andreatta <thaisandreatta@gmail.com>
2023-07-04 11:49:03 -0300

    app pronto

--- tweet.py
+++ tweet.py
@@ -1,26 +1,25 @@
 import tweepy
 import requests
 import time
-
-# Configurar as chaves de acesso do Twitter
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAABZTogEAAAAANcav5xsdavU3JwHudWGEmhR%2Boz0%3D7bOmeDssNafdHyBsNhMKeov0KDshgR8VekK5SZfsh1bxRnZg8y'
-access_token = '1637446359333699585-on06KcLHRXkSQ6r5SvDdRAirf8j1lZ'
-access_token_secret = 'XcGOYFf10KvNX9dYtcbbX59iF4e9FfXHXHhheVvCdRB7e'
-
-# Configurar a chave de API do OpenWeatherMap
-api_key = 'd5e40bae38cd01caf620c5aa0a9f40a3'
+import config 
 
 # Autenticar o bot com o Twitter
-auth = tweepy.OAuth2BearerHandler(bearer_token)
+client = tweepy.Client(consumer_key=config.API_KEY, consumer_secret=config.API_SECRET, access_token=config.ACCESS_TOKEN, access_token_secret=config.ACCESS_TOKEN_SECRET)
+auth = tweepy.OAuth1UserHandler(consumer_key=config.API_KEY, consumer_secret=config.API_SECRET, access_token=config.ACCESS_TOKEN, access_token_secret=config.ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 # Variáveis para armazenar a última temperatura twittada
 last_temperature = None
 
+# Função para twittar usando a API do Twitter
+def tweet(text):
+    response = client.create_tweet(text=text)
+    print(response)
+
 while True:
     try:
         # Obter a temperatura atual de Belo Horizonte usando a API OpenWeatherMap
-        url = f"http://api.openweathermap.org/data/2.5/weather?q=Belo%20Horizonte,BR&appid={api_key}&units=metric"
+        url = f"http://api.openweathermap.org/data/2.5/weather?q=Belo%20Horizonte,BR&appid={config.OPENWEATHER_API}&units=metric"
         response = requests.get(url)
 
         # Verificar o código de status da resposta
@@ -30,12 +29,12 @@ while True:
             # Verificar se os dados estão presentes
             if 'main' in data:
                 temperature = data['main']['temp']
-                
+
                 # Comparar com a temperatura anteriormente twittada
                 if temperature != last_temperature:
                     # Tweetar a nova temperatura no Twitter
-                    tweet = f"A temperatura atual em Belo Horizonte é de {temperature}°C."
-                    api.update_status(tweet)
+                    tweet_text = f"A temperatura atual em Belo Horizonte é de {temperature}°C."
+                    tweet(tweet_text)
                     last_temperature = temperature
             else:
                 print("Erro: Dados não encontrados na resposta da API.")
@@ -44,5 +43,5 @@ while True:
     except Exception as e:
         print(f"Ocorreu um erro: {str(e)}")
 
-    # Aguardar 5 minutos antes de verificar novamente a temperatura
-    time.sleep(300)
+    # Aguardar 30 segundos antes de verificar novamente a temperatura
+    time.sleep(30)
